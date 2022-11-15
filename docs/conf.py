ec0d44d67d589d1f04497753b6791757ad553457
Benjamin T. Vincent <inferencelab@gmail.com>
2022-11-15 09:08:45 +0000

    #53 readthedocs. Another fix to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../mypackage"))
+sys.path.insert(0, os.path.abspath("../causalpy"))
 
 
 # -- Project information -----------------------------------------------------
