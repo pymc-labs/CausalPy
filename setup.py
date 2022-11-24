5a74cd89c0dca9ec49a3488f544891164a1a77aa
juanitorduz <juan.orduz@wolt.com>
2022-11-24 09:42:10 +0100

    fix version setting

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ import os
 
 from setuptools import find_packages, setup
 
-from causalpy.version import __version__
-
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 README_FILE = os.path.join(PROJECT_ROOT, "README.md")
-VERSION_FILE = os.path.join(PROJECT_ROOT, "bambi", "version.py")
+VERSION_FILE = os.path.join(PROJECT_ROOT, "causalpy", "version.py")
 REQUIREMENTS_FILE = os.path.join(PROJECT_ROOT, "requirements.txt")
+TEST_REQUIREMENTS_FILE = os.path.join(PROJECT_ROOT, "requirements-test.txt")
 
 
 def get_long_description():
@@ -15,18 +14,18 @@ def get_long_description():
         return f.read()
 
 
-# get version
-# sys.path.insert(0, os.path.abspath("../"))
-# from causalpy.version import __version__
-exec(open("causalpy/version.py").read())
-
+with open(VERSION_FILE) as f:
+    version = f.read().split("=")[-1].strip().strip('"')
 
 with open(REQUIREMENTS_FILE) as f:
     install_reqs = f.read().splitlines()
 
+with open(TEST_REQUIREMENTS_FILE) as f:
+    tests_reqs = f.read().splitlines()
+
 setup(
     name="CausalPy",
-    version=__version__,
+    version=version,
     description="Causal inference for quasi-experiments in Python",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
@@ -37,5 +36,5 @@ setup(
     python_requires=">=3.8",
     maintainer="Benjamin T. Vincent",
     install_requires=install_reqs,
-    # tests_require=test_reqs,
+    tests_require=tests_reqs,
 )
