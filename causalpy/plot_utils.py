c98cfd9070225d11cc8d572556e613350bb4dc64
Benjamin T. Vincent <inferencelab@gmail.com>
2022-12-27 20:43:24 +0000

    improve plots + plot posterior mu rather than posterior predictive

--- causalpy/plot_utils.py
+++ causalpy/plot_utils.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import xarray as xr
+from matplotlib.collections import PolyCollection
 
 
 def plot_xY(
@@ -13,28 +14,46 @@ def plot_xY(
     ax: plt.Axes,
     plot_hdi_kwargs: Optional[Dict[str, Any]] = None,
     hdi_prob: float = 0.94,
+    label: Optional[str] = "",
     include_label: bool = True,
-) -> None:
+):
     """Utility function to plot HDI intervals."""
 
     if plot_hdi_kwargs is None:
         plot_hdi_kwargs = {}
 
-    az.plot_hdi(
+    (h_line,) = ax.plot(
+        x,
+        Y.mean(dim=["chain", "draw"]),
+        ls="-",
+        **plot_hdi_kwargs,
+        label=f"{label}" if include_label else None,
+    )
+    ax_hdi = az.plot_hdi(
         x,
         Y,
         hdi_prob=hdi_prob,
         fill_kwargs={
             "alpha": 0.25,
-            "label": f"{hdi_prob*100}% HDI" if include_label else None,
+            "label": " ",  # f"{hdi_prob*100}% HDI" if include_label else None,
         },
         smooth=False,
         ax=ax,
         **plot_hdi_kwargs,
     )
-    ax.plot(
-        x,
-        Y.mean(dim=["chain", "draw"]),
-        color="k",
-        label="Posterior mean" if include_label else None,
-    )
+    # Return handle to patch.
+    # We get a list of the childen of the axis
+    # Filter for just the PolyCollection objects
+    # Take the last one
+    h_patch = list(
+        filter(lambda x: isinstance(x, PolyCollection), ax_hdi.get_children())
+    )[-1]
+
+    # if include_label:
+    #     handles, labels = ax.get_legend_handles_labels()
+    #     ax.legend(
+    #         handles=[(h1, h2) for h1, h2 in zip(handles[::2], handles[1::2])],
+    #         # labels=[l1 + " + " + l2 for l1, l2 in zip(labels[::2], labels[1::2])],
+    #         labels=[l1 for l1 in labels[::2]],
+    #     )
+    return h_line, h_patch
