b8a3dff265b7698e01944b50d21eb7841bf00fb7
Kadlicskó Máté <mtkama28@mtu.gov.hu>
2023-02-27 13:56:01 +0100

    mostly stylistic changes

--- causalpy/skl_meta_learners.py
+++ causalpy/skl_meta_learners.py
@@ -1,61 +1,52 @@
-import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.utils import check_consistent_length
+import pandas as pd
 from sklearn.base import clone
 from sklearn.linear_model import LogisticRegression
+from sklearn.utils import check_consistent_length
+
 from causalpy.utils import _is_variable_dummy_coded
 
 
 class MetaLearner:
-    """
-    Base class for meta-learners. 
-    """
+    "Base class for meta-learners."
 
-    def __init__(
-        self,
-        X: pd.DataFrame,
-        y: pd.Series,
-        treated: pd.Series
-    ) -> None:
+    def __init__(self, X: pd.DataFrame, y: pd.Series, treated: pd.Series) -> None:
         # Check whether input is appropriate
         check_consistent_length(X, y, treated)
         if not _is_variable_dummy_coded(treated):
             raise ValueError('Treatment variable is not dummy coded.')
 
+        self.cate = None
         self.treated = treated
         self.X = X
         self.y = y
 
     def predict_cate(self, X: pd.DataFrame) -> np.array:
-        """
-        Predict conditional average treatment effect for given input X.
-        """
+        "Predict conditional average treatment effect for given input X."
         raise NotImplementedError()
 
     def predict_ate(self, X: pd.DataFrame) -> np.float64:
-        """
-        Predict average treatment effect for given input X.
-        """
+        "Predict average treatment effect for given input X."
         return self.predict_cate(X).mean()
 
-    def bootstrap(self,
-                  X_ins: pd.DataFrame,
-                  y: pd.Series,
-                  treated: pd.Series,
-                  X: None,
-                  frac_samples: float = None,
-                  n_samples: int = 1000,
-                  n_iter: int = 1000
-                  ) -> np.array:
+    def bootstrap(
+        self,
+        X_ins: pd.DataFrame,
+        y: pd.Series,
+        treated: pd.Series,
+        X: None,
+        frac_samples: float = None,
+        n_samples: int = 1000,
+        n_iter: int = 1000
+    ) -> np.array:
         """
         Runs bootstrap n_iter times on a sample of size n_samples.
         Fits on (X_ins, y, treated), then predicts on X.
         """
         results = []
         for _ in range(n_iter):
-            X_bs = X_ins.sample(frac=frac_samples,
-                                n=n_samples,
-                                replace=True)
+            X_bs = X_ins.sample(frac=frac_samples, n=n_samples, replace=True)
             y_bs = y.loc[X_bs.index].reset_index(drop=True)
             t_bs = treated.loc[X_bs.index].reset_index(drop=True)
 
@@ -65,83 +56,73 @@ class MetaLearner:
 
         return np.array(results)
 
-    def ate_confidence_interval(self,
-                                X_ins: pd.DataFrame,
-                                y: pd.Series,
-                                treated: pd.Series,
-                                X: None,
-                                q: float = .95,
-                                frac_samples: float = None,
-                                n_samples: int = 1000,
-                                n_iter: int = 1000):
-        """
-        Estimates confidence intervals for ATE on X using bootstraping. 
-        """
-        cates = self.bootstrap(X_ins,
-                               y,
-                               treated,
-                               X,
-                               frac_samples,
-                               n_samples,
-                               n_iter)
-        return np.quantile(cates, q=q), np.quantile(cates, q=1-q)
+    def ate_confidence_interval(
+        self,
+        X_ins: pd.DataFrame,
+        y: pd.Series,
+        treated: pd.Series,
+        X: None,
+        q: float = .95,
+        frac_samples: float = None,
+        n_samples: int = 1000,
+        n_iter: int = 1000
+    ):
+        "Estimates confidence intervals for ATE on X using bootstraping."
+        cates = self.bootstrap(X_ins, y, treated, X, frac_samples, n_samples, n_iter)
+        return np.quantile(cates, q=q), np.quantile(cates, q=1 - q)
 
-    def cate_confidence_interval(self,
-                                 X_ins: pd.DataFrame,
-                                 y: pd.Series,
-                                 treated: pd.Series,
-                                 X: None,
-                                 q: float = .95,
-                                 frac_samples: float = None,
-                                 n_samples: int = 1000,
-                                 n_iter: int = 1000):
-        """
-        Estimates confidence intervals for CATE on X using bootstraping. 
-        """
-        cates = self.bootstrap(X_ins,
-                               y,
-                               treated,
-                               X,
-                               frac_samples,
-                               n_samples,
-                               n_iter)
-        conf_ints = np.append(np.quantile(cates, q, axis=0).reshape(-1, 1),
-                              np.quantile(cates, 1 - q, axis=0).reshape(-1, 1),
-                              axis=1)
+
+    def cate_confidence_interval(
+        self,
+        X_ins: pd.DataFrame,
+        y: pd.Series,
+        treated: pd.Series,
+        X: None,
+        q: float = .95,
+        frac_samples: float = None,
+        n_samples: int = 1000,
+        n_iter: int = 1000
+    ):
+        "Estimates confidence intervals for CATE on X using bootstraping."
+        cates = self.bootstrap(X_ins, y, treated, X, frac_samples, n_samples, n_iter)
+        conf_ints = np.append(
+            np.quantile(cates, q, axis=0).reshape(-1, 1),
+            np.quantile(cates, 1 - q, axis=0).reshape(-1, 1),
+            axis=1,
+        )
         return conf_ints
 
-    def fit(self,
-            X: pd.DataFrame,
-            y: pd.Series,
-            treated: pd.Series):
+    def fit(self, X: pd.DataFrame, y: pd.Series, treated: pd.Series):
         "Fits model."
         raise NotImplementedError()
 
     def summary(self):
-        "Prints summary. Conent is undecided yet."
-        raise NotImplementedError()
+        "Prints summary."
+        conf_ints = self.ate_confidence_interval(self.X, self.y, self.treated, self.X)
+        print(f"Number of observations:            {self.X.shape[0]}")
+        print(f"Number of treated observations:    {self.treated.sum()}")
+        print(f"Average treatement effect (ATE):   {self.predict_ate(self.X.shape[0])}")
+        print(f"Confidence interval for ATE:       {conf_ints}")
 
     def plot(self):
         "Plots results. Content is undecided yet."
-        raise NotImplementedError()
+        plt.hist(self.cate, bins=40, density=True)
 
 
 class SLearner(MetaLearner):
     """
     Implements of S-learner described in [1]. S-learner estimates conditional average
-    treatment effect with the use of a single model. 
+    treatment effect with the use of a single model.
 
-    [1] Künzel, Sören R., Jasjeet S. Sekhon, Peter J. Bickel, and Bin Yu. 
+    [1] Künzel, Sören R., Jasjeet S. Sekhon, Peter J. Bickel, and Bin Yu.
         Metalearners for estimating heterogeneous treatment effects using machine learning.
         Proceedings of the national academy of sciences 116, no. 10 (2019): 4156-4165.
 
     """
 
-    def __init__(self,
-                 X: pd.DataFrame,
-                 y: pd.Series,
-                 treated: pd.Series,
-                 model) -> None:
+    def __init__(
+        self, X: pd.DataFrame, y: pd.Series, treated: pd.Series, model
+    ) -> None:
         super().__init__(X=X, y=y, treated=treated)
         self.model = model
         self.fit(X, y, treated)
@@ -150,8 +131,8 @@ class SLearner(MetaLearner):
     def fit(self, X: pd.DataFrame,
             y: pd.Series,
             treated: pd.Series):
-        X_T = X.assign(treatment=treated)
-        self.model = self.model.fit(X_T, y)
+        X_t = X.assign(treatment=treated)
+        self.model = self.model.fit(X_t, y)
         return self
 
     def predict_cate(self, X: pd.DataFrame) -> np.array:
@@ -163,84 +144,91 @@ class SLearner(MetaLearner):
 class TLearner(MetaLearner):
     """
     Implements of T-learner described in [1]. T-learner fits two separate models to estimate
-    conditional average treatment effect. 
+    conditional average treatment effect.
 
-    [1] Künzel, Sören R., Jasjeet S. Sekhon, Peter J. Bickel, and Bin Yu. 
+    [1] Künzel, Sören R., Jasjeet S. Sekhon, Peter J. Bickel, and Bin Yu.
         Metalearners for estimating heterogeneous treatment effects using machine learning.
         Proceedings of the national academy of sciences 116, no. 10 (2019): 4156-4165.
 
     """
 
-    def __init__(self,
-                 X: pd.DataFrame,
-                 y: pd.Series,
-                 treated: pd.Series,
-                 model=None,
-                 treated_model=None,
-                 untreated_model=None
-                 ) -> None:
+    def __init__(
+        self,
+        X: pd.DataFrame,
+        y: pd.Series,
+        treated: pd.Series,
+        model=None,
+        treated_model=None,
+        untreated_model=None
+    ) -> None:
         super().__init__(X=X, y=y, treated=treated)
 
         if model is None and (untreated_model is None or treated_model is None):
-            raise (ValueError("Either model or both of treated_model and untreated_model \
-            have to be specified."))
+            raise ValueError(
+                "Either model or both of treated_model and untreated_model \
+                have to be specified."
+                )
         elif not (model is None or untreated_model is None or treated_model is None):
-            raise (ValueError("Either model or both of treated_model and untreated_model \
-            have to be specified."))
+            raise ValueError(
+                "Either model or both of treated_model and untreated_model \
+                have to be specified."
+                )
 
         if model is not None:
             untreated_model = clone(model)
             treated_model = clone(model)
 
-        self.models = {'treated': treated_model,
-                       'untreated': untreated_model}
+        self.models = {"treated": treated_model, "untreated": untreated_model}
 
         self.fit(X, y, treated)
         self.cate = self.predict_cate(X)
 
-    def fit(self, X: pd.DataFrame,
-            y: pd.Series,
-            treated: pd.Series):
-        self.models['treated'].fit(X[treated == 1], y[treated == 1])
-        self.models['untreated'].fit(X[treated == 0], y[treated == 0])
+    def fit(self, X: pd.DataFrame, y: pd.Series, treated: pd.Series):
+        self.models["treated"].fit(X[treated == 1], y[treated == 1])
+        self.models["untreated"].fit(X[treated == 0], y[treated == 0])
         return self
 
     def predict_cate(self, X: pd.DataFrame) -> np.array:
-        treated_model = self.models['treated']
-        untreated_model = self.models['untreated']
+        treated_model = self.models["treated"]
+        untreated_model = self.models["untreated"]
         return treated_model.predict(X) - untreated_model.predict(X)
 
 
 class XLearner(MetaLearner):
     """
     Implements of X-learner introduced in [1]. X-learner estimates conditional average treatment
-    effect with the use of five separate models. 
+    effect with the use of five separate models.
 
-    [1] Künzel, Sören R., Jasjeet S. Sekhon, Peter J. Bickel, and Bin Yu. 
+    [1] Künzel, Sören R., Jasjeet S. Sekhon, Peter J. Bickel, and Bin Yu.
         Metalearners for estimating heterogeneous treatment effects using machine learning.
         Proceedings of the national academy of sciences 116, no. 10 (2019): 4156-4165.
 
     """
 
-    def __init__(self,
-                 X,
-                 y,
-                 treated,
-                 model=None,
-                 treated_model=None,
-                 untreated_model=None,
-                 treated_cate_estimator=None,
-                 untreated_cate_estimator=None,
-                 propensity_score_model=None
-                 ):
+    def __init__(
+        self,
+        X,
+        y,
+        treated,
+        model=None,
+        treated_model=None,
+        untreated_model=None,
+        treated_cate_estimator=None,
+        untreated_cate_estimator=None,
+        propensity_score_model=None
+    ):
         super().__init__(X=X, y=y, treated=treated)
 
         if model is None and (untreated_model is None or treated_model is None):
-            raise ValueError("""Either model or each of treated_model, untreated_model, \
-            treated_cate_estimator, untreated_cate_estimator has to be specified.""")
+            raise ValueError(
+                "Either model or each of treated_model, untreated_model, \
+                treated_cate_estimator, untreated_cate_estimator has to be specified."
+            )
         elif not (model is None or untreated_model is None or treated_model is None):
-            raise ValueError("Either model or each of treated_model, untreated_model, \
-            treated_cate_estimator, untreated_cate_estimator has to be specified.")
+            raise ValueError(
+                "Either model or each of treated_model, untreated_model, \
+                treated_cate_estimator, untreated_cate_estimator has to be specified."
+            )
 
         if propensity_score_model is None:
             propensity_score_model = LogisticRegression(penalty=None)
@@ -251,30 +239,31 @@ class XLearner(MetaLearner):
             treated_cate_estimator = clone(model)
             untreated_cate_estimator = clone(model)
 
-        self.models = {'treated': treated_model,
-                       'untreated': untreated_model,
-                       'treated_cate': treated_cate_estimator,
-                       'untreated_cate': untreated_cate_estimator,
-                       'propensity': propensity_score_model
-                       }
+        self.models = {
+            "treated": treated_model,
+            "untreated": untreated_model,
+            "treated_cate": treated_cate_estimator,
+            "untreated_cate": untreated_cate_estimator,
+            "propensity": propensity_score_model    
+        }
 
         self.fit(X, y, treated)
 
         # Compute cate
         cate_t = treated_cate_estimator.predict(X)
         cate_u = treated_cate_estimator.predict(X)
-        g = self.models['propensity'].predict(X)
+        g = self.models["propensity"].predict(X)
 
         self.cate = g * cate_u + (1 - g) * cate_t
 
-    def fit(self, X: pd.DataFrame,
-            y: pd.Series,
-            treated: pd.Series):
-        (treated_model,
-         untreated_model,
-         treated_cate_estimator,
-         untreated_cate_estimator,
-         propensity_score_model) = self.models.values()
+    def fit(self, X: pd.DataFrame, y: pd.Series, treated: pd.Series):
+        (
+            treated_model,
+            untreated_model,
+            treated_cate_estimator,
+            untreated_cate_estimator,
+            propensity_score_model
+        ) = self.models.values()
 
         # Split data to treated and untreated subsets
         X_t, y_t = X[treated == 1], y[treated == 1]
@@ -296,10 +285,10 @@ class XLearner(MetaLearner):
         return self
 
     def predict_cate(self, X):
-        cate_estimate_treated = self.models['treated_cate'].predict(X)
-        cate_estimate_untreated = self.models['untreated_cate'].predict(X)
+        cate_estimate_treated = self.models["treated_cate"].predict(X)
+        cate_estimate_untreated = self.models["untreated_cate"].predict(X)
 
-        g = self.models['propensity'].predict_proba(X)[:, 1]
+        g = self.models["propensity"].predict_proba(X)[:, 1]
 
         return g * cate_estimate_untreated + (1 - g) * cate_estimate_treated
 
@@ -310,23 +299,28 @@ class DRLearner(MetaLearner):
 
     """
 
-    def __init__(self,
-                 X,
-                 y,
-                 treated,
-                 model=None,
-                 treated_model=None,
-                 untreated_model=None,
-                 propensity_score_model=None
-                 ):
+    def __init__(
+        self,
+        X,
+        y,
+        treated,
+        model=None,
+        treated_model=None,
+        untreated_model=None,
+        propensity_score_model=None
+    ):
         super().__init__(X=X, y=y, treated=treated)
 
         if model is None and (untreated_model is None or treated_model is None):
-            raise ValueError("""Either model or each of treated_model, untreated_model, \
-            treated_cate_estimator, untreated_cate_estimator has to be specified.""")
+            raise ValueError(
+                "Either model or each of treated_model, untreated_model, \
+                treated_cate_estimator, untreated_cate_estimator has to be specified."
+                )
         elif not (model is None or untreated_model is None or treated_model is None):
-            raise ValueError("Either model or each of treated_model, untreated_model, \
-            treated_cate_estimator, untreated_cate_estimator has to be specified.")
+            raise ValueError(
+                "Either model or each of treated_model, untreated_model, \
+                treated_cate_estimator, untreated_cate_estimator has to be specified."
+                )
 
         if propensity_score_model is None:
             propensity_score_model = LogisticRegression(penalty=None)
@@ -336,30 +330,28 @@ class DRLearner(MetaLearner):
             untreated_model = clone(model)
 
         # Estimate response function
-        self.models = {'treated': treated_model,
-                       'untreated': untreated_model,
-                       'propensity': propensity_score_model}
+        self.models = {
+            "treated": treated_model,
+            "untreated": untreated_model,
+            "propensity": propensity_score_model
+            }
 
         self.fit(X, y, treated)
 
         # Estimate CATE
-        g = self.models['propensity'].predict_proba(X)[:, 1]
+        g = self.models["propensity"].predict_proba(X)[:, 1]
         m0 = untreated_model.predict(X)
         m1 = treated_model.predict(X)
 
         self.cate = (treated * (y - m1) / g + m1
                      - ((1 - treated) * (y - m0) / (1 - g) + m0))
 
-    def fit(self, X: pd.DataFrame,
-            y: pd.Series,
-            treated: pd.Series):
+    def fit(self, X: pd.DataFrame, y: pd.Series, treated: pd.Series):
         # Split data to treated and untreated subsets
         X_t, y_t = X[treated == 1], y[treated == 1]
         X_u, y_u = X[treated == 0], y[treated == 0]
 
-        (treated_model,
-         untreated_model,
-         propensity_score_model) = self.models.values()
+        treated_model, untreated_model, propensity_score_model = self.models.values()
 
         # Estimate response functions
         treated_model.fit(X_t, y_t)
@@ -371,4 +363,6 @@ class DRLearner(MetaLearner):
         return self
 
     def predict_cate(self, X):
-        return self.cate
+        m1 = self.models["treated"].predict(X)
+        m0 = self.models["untreated"].predict(X)
+        return m1 - m0 
