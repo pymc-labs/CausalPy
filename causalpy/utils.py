542e129f03817776daedf766f444b3707a242cf3
Kadlicskó Máté <kadlicsko.mate@gmail.com>
2023-03-01 16:56:30 +0100

    added _fit function to reduce boilerplate code

--- causalpy/utils.py
+++ causalpy/utils.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
+from causalpy.pymc_models import ModelBuilder
+
+
+def _fit(model, X, y, coords):
+    """Fits model to X, y, where model is either a sklearn model or a ModelBuilder
+    instance. In the later case it passes coords, in the first case coords is ignored."""
+    if isinstance(model, ModelBuilder):
+        model.fit(X, y, coords)
+    else:
+        model.fit(X, y)
+
 
 def _is_variable_dummy_coded(series: pd.Series) -> bool:
     """Check if a data in the provided Series is dummy coded. It should be 0 or 1
