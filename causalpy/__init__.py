d88472c2fec255a41a209abc8e84dc7cc362139d
Kadlicskó Máté <kadlicsko.mate@gmail.com>
2023-04-09 18:54:50 +0200

    imported summary

--- causalpy/__init__.py
+++ causalpy/__init__.py
@@ -5,6 +5,7 @@ import causalpy.pymc_models
 import causalpy.skl_experiments
 import causalpy.skl_meta_learners
 import causalpy.skl_models
+import causalpy.summary
 from causalpy.version import __version__
 
 from .data import load_data
