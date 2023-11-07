5cb12b18986698669cc6782b414e89cfdbb7eb6b
juanitorduz <juan.orduz@wolt.com>
2023-11-07 10:14:43 +0100

    adopt ruff

--- causalpy/__init__.py
+++ causalpy/__init__.py
@@ -1,11 +1,20 @@
 import arviz as az
 
-import causalpy.pymc_experiments
-import causalpy.pymc_models
-import causalpy.skl_experiments
-import causalpy.skl_models
+from causalpy import pymc_experiments
+from causalpy import pymc_models
+from causalpy import skl_experiments
+from causalpy import skl_models
 from causalpy.version import __version__
 
 from .data import load_data
 
 az.style.use("arviz-darkgrid")
+
+__all__ = [
+    "pymc_experiments",
+    "pymc_models",
+    "skl_experiments",
+    "skl_models",
+    "load_data",
+    "__version__",
+]
