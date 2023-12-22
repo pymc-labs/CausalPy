a241150f8fe71f8d5d160e4ef3f62c9e0b6c7f56
Benjamin T. Vincent <inferencelab@gmail.com>
2023-12-22 15:07:41 +0000

    use 1 decimal place in the docstring output

--- causalpy/pymc_experiments.py
+++ causalpy/pymc_experiments.py
@@ -1239,10 +1239,10 @@ class PrePostNEGD(ExperimentalDesign):
     Results:
     Causal impact = 1.88, $CI_{94%}$[1.7, 2.1]
     Model coefficients:
-    Intercept                     -0.47, 94% HDI [-1.18, 0.24]
-    C(group)[T.1]                 1.88, 94% HDI [1.69, 2.08]
-    pre                           1.05, 94% HDI [0.98, 1.12]
-    sigma                         0.51, 94% HDI [0.46, 0.56]
+    Intercept                     -0.4, 94% HDI [-1.1, 0.2]
+    C(group)[T.1]                 1.8, 94% HDI [1.6, 2.0]
+    pre                           1.0, 94% HDI [0.9, 1.1]
+    sigma                         0.5, 94% HDI [0.4, 0.5]
     """
 
     def __init__(
