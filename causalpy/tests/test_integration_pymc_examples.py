85dbd748e482f58174aefcec156dbe1737d3a7dd
Nathaniel <NathanielF@users.noreply.github.com>
2024-06-08 08:14:07 +0100

    running ppc checks on iv test
    Signed-off-by: Nathaniel <NathanielF@users.noreply.github.com>

--- causalpy/tests/test_integration_pymc_examples.py
+++ causalpy/tests/test_integration_pymc_examples.py
@@ -504,7 +504,8 @@ def test_iv_reg():
             sample_kwargs=sample_kwargs
         ),
     )
-    result.model.sample_predictive_distribution(ppc_sampler=None)
+    result.model.sample_predictive_distribution(ppc_sampler="jax")
+    result.model.sample_predictive_distribution(ppc_sampler="pymc")
     assert isinstance(df, pd.DataFrame)
     assert isinstance(data, pd.DataFrame)
     assert isinstance(instruments_data, pd.DataFrame)
