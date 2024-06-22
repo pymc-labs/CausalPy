79eb4c583ba6ec48179c5b2836c4a06354aafcce
Carlos Trujillo <59846724+cetagostini@users.noreply.github.com>
2024-06-23 02:32:19 +0300

    Bayesian ROPE Analysis

--- causalpy/pymc_experiments.py
+++ causalpy/pymc_experiments.py
@@ -49,6 +49,7 @@ from causalpy.data_validation import (
 )
 from causalpy.plot_utils import plot_xY
 from causalpy.utils import round_num
+from causalpy.pymc_rope import ROPE
 
 LEGEND_FONT_SIZE = 12
 az.style.use("arviz-darkgrid")
@@ -356,7 +357,7 @@ class PrePostFit(ExperimentalDesign, PrePostFitDataValidator):
         self.print_coefficients(round_to)
 
 
-class InterruptedTimeSeries(PrePostFit):
+class InterruptedTimeSeries(PrePostFit, ROPE):
     """
     A wrapper around PrePostFit class
 
@@ -396,7 +397,7 @@ class InterruptedTimeSeries(PrePostFit):
     expt_type = "Interrupted Time Series"
 
 
-class SyntheticControl(PrePostFit):
+class SyntheticControl(PrePostFit, ROPE):
     """A wrapper around the PrePostFit class
 
     :param data:
