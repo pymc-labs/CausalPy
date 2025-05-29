14afe0911ae94b8286463e43f52855f12467d766
Jean Van Dyk <jeanvandyk14@gmail.com>
2025-05-29 10:43:24 +0200

    Minor fix in docstring

--- causalpy/pymc_models.py
+++ causalpy/pymc_models.py
@@ -541,7 +541,8 @@ class InterventionTimeEstimator(PyMCModel):
         ...     t,
         ...     y,
         ...     coords,
-        ...     effect=["impulse"])
+        ...     effect=["impulse"]
+        ...     n=5000)
         Inference data...
     """
 
@@ -600,11 +601,11 @@ class InterventionTimeEstimator(PyMCModel):
             # --- Likelihood ---
             pm.Normal("y_hat", mu=mu, sigma=2, observed=y)
 
-    def fit(self, t, y, coords, effect=[], span=None, grain_season=1):
+    def fit(self, t, y, coords, effect=[], span=None, grain_season=1, n=1000):
         """
         Draw samples from posterior distribution
         """
         self.build_model(t, y, coords, effect, span, grain_season)
         with self:
-            self.idata = pm.sample(**self.sample_kwargs)
+            self.idata = pm.sample(n, **self.sample_kwargs)
         return self.idata
