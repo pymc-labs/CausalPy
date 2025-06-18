5039fda74f2108c4ae8fdc90d36f97eb7bd3f8f1
Jean Van Dyk <jeanvandyk14@gmail.com>
2025-06-18 15:41:42 +0200

    fixing merging issues

--- causalpy/pymc_models.py
+++ causalpy/pymc_models.py
@@ -19,6 +19,7 @@ import arviz as az
 import numpy as np
 import pandas as pd
 import pymc as pm
+import pytensor.tensor as pt
 import xarray as xr
 from arviz import r2_score
 
@@ -288,6 +289,224 @@ class WeightedSumFitter(PyMCModel):
             sigma = pm.HalfNormal("sigma", 1)
             mu = pm.Deterministic("mu", pm.math.dot(X, beta), dims="obs_ind")
             pm.Normal("y_hat", mu, sigma, observed=y, dims="obs_ind")
+
+
+class InstrumentalVariableRegression(PyMCModel):
+    """Custom PyMC model for instrumental linear regression
+
+    Example
+    --------
+    >>> import causalpy as cp
+    >>> import numpy as np
+    >>> from causalpy.pymc_models import InstrumentalVariableRegression
+    >>> N = 10
+    >>> e1 = np.random.normal(0, 3, N)
+    >>> e2 = np.random.normal(0, 1, N)
+    >>> Z = np.random.uniform(0, 1, N)
+    >>> ## Ensure the endogeneity of the the treatment variable
+    >>> X = -1 + 4 * Z + e2 + 2 * e1
+    >>> y = 2 + 3 * X + 3 * e1
+    >>> t = X.reshape(10, 1)
+    >>> y = y.reshape(10, 1)
+    >>> Z = np.asarray([[1, Z[i]] for i in range(0, 10)])
+    >>> X = np.asarray([[1, X[i]] for i in range(0, 10)])
+    >>> COORDS = {"instruments": ["Intercept", "Z"], "covariates": ["Intercept", "X"]}
+    >>> sample_kwargs = {
+    ...     "tune": 5,
+    ...     "draws": 10,
+    ...     "chains": 2,
+    ...     "cores": 2,
+    ...     "target_accept": 0.95,
+    ...     "progressbar": False,
+    ... }
+    >>> iv_reg = InstrumentalVariableRegression(sample_kwargs=sample_kwargs)
+    >>> iv_reg.fit(
+    ...     X,
+    ...     Z,
+    ...     y,
+    ...     t,
+    ...     COORDS,
+    ...     {
+    ...         "mus": [[-2, 4], [0.5, 3]],
+    ...         "sigmas": [1, 1],
+    ...         "eta": 2,
+    ...         "lkj_sd": 1,
+    ...     },
+    ...     None,
+    ... )
+    Inference data...
+    """
+
+    def build_model(self, X, Z, y, t, coords, priors):
+        """Specify model with treatment regression and focal regression data and priors
+
+        :param X: A pandas dataframe used to predict our outcome y
+        :param Z: A pandas dataframe used to predict our treatment variable t
+        :param y: An array of values representing our focal outcome y
+        :param t: An array of values representing the treatment t of
+                  which we're interested in estimating the causal impact
+        :param coords: A dictionary with the coordinate names for our
+                       instruments and covariates
+        :param priors: An optional dictionary of priors for the mus and
+                      sigmas of both regressions
+                      :code:`priors = {"mus": [0, 0], "sigmas": [1, 1],
+                      "eta": 2, "lkj_sd": 2}`
+        """
+
+        # --- Priors ---
+        with self:
+            self.add_coords(coords)
+            beta_t = pm.Normal(
+                name="beta_t",
+                mu=priors["mus"][0],
+                sigma=priors["sigmas"][0],
+                dims="instruments",
+            )
+            beta_z = pm.Normal(
+                name="beta_z",
+                mu=priors["mus"][1],
+                sigma=priors["sigmas"][1],
+                dims="covariates",
+            )
+            sd_dist = pm.Exponential.dist(priors["lkj_sd"], shape=2)
+            chol, corr, sigmas = pm.LKJCholeskyCov(
+                name="chol_cov",
+                eta=priors["eta"],
+                n=2,
+                sd_dist=sd_dist,
+            )
+            # compute and store the covariance matrix
+            pm.Deterministic(name="cov", var=pt.dot(l=chol, r=chol.T))
+
+            # --- Parameterization ---
+            mu_y = pm.Deterministic(name="mu_y", var=pm.math.dot(X, beta_z))
+            # focal regression
+            mu_t = pm.Deterministic(name="mu_t", var=pm.math.dot(Z, beta_t))
+            # instrumental regression
+            mu = pm.Deterministic(name="mu", var=pt.stack(tensors=(mu_y, mu_t), axis=1))
+
+            # --- Likelihood ---
+            pm.MvNormal(
+                name="likelihood",
+                mu=mu,
+                chol=chol,
+                observed=np.stack(arrays=(y.flatten(), t.flatten()), axis=1),
+                shape=(X.shape[0], 2),
+            )
+
+    def sample_predictive_distribution(self, ppc_sampler="jax"):
+        """Function to sample the Multivariate Normal posterior predictive
+        Likelihood term in the IV class. This can be slow without
+        using the JAX sampler compilation method. If using the
+        JAX sampler it will sample only the posterior predictive distribution.
+        If using the PYMC sampler if will sample both the prior
+        and posterior predictive distributions."""
+        random_seed = self.sample_kwargs.get("random_seed", None)
+
+        if ppc_sampler == "jax":
+            with self:
+                self.idata.extend(
+                    pm.sample_posterior_predictive(
+                        self.idata,
+                        random_seed=random_seed,
+                        compile_kwargs={"mode": "JAX"},
+                    )
+                )
+        elif ppc_sampler == "pymc":
+            with self:
+                self.idata.extend(pm.sample_prior_predictive(random_seed=random_seed))
+                self.idata.extend(
+                    pm.sample_posterior_predictive(
+                        self.idata,
+                        random_seed=random_seed,
+                    )
+                )
+
+    def fit(self, X, Z, y, t, coords, priors, ppc_sampler=None):
+        """Draw samples from posterior distribution and potentially
+        from the prior and posterior predictive distributions. The
+        fit call can take values for the
+        ppc_sampler = ['jax', 'pymc', None]
+        We default to None, so the user can determine if they wish
+        to spend time sampling the posterior predictive distribution
+        independently.
+        """
+
+        # Ensure random_seed is used in sample_prior_predictive() and
+        # sample_posterior_predictive() if provided in sample_kwargs.
+        # Use JAX for ppc sampling of multivariate likelihood
+
+        self.build_model(X, Z, y, t, coords, priors)
+        with self:
+            self.idata = pm.sample(**self.sample_kwargs)
+        self.sample_predictive_distribution(ppc_sampler=ppc_sampler)
+        return self.idata
+
+
+class PropensityScore(PyMCModel):
+    r"""
+    Custom PyMC model for inverse propensity score models
+
+    .. note:
+        Generally, the `.fit()` method should be used rather than
+        calling `.build_model()` directly.
+
+    Defines the PyMC model
+
+    .. math::
+        \beta &\sim \mathrm{Normal}(0, 1) \\
+        \sigma &\sim \mathrm{HalfNormal}(1) \\
+        \mu &= X \cdot \beta \\
+        p &= \text{logit}^{-1}(\mu) \\
+        t &\sim \mathrm{Bernoulli}(p)
+
+    Example
+    --------
+    >>> import causalpy as cp
+    >>> import numpy as np
+    >>> from causalpy.pymc_models import PropensityScore
+    >>> df = cp.load_data('nhefs')
+    >>> X = df[["age", "race"]]
+    >>> t = np.asarray(df["trt"])
+    >>> ps = PropensityScore(sample_kwargs={"progressbar": False})
+    >>> ps.fit(X, t, coords={
+    ...                 'coeffs': ['age', 'race'],
+    ...                 'obs_ind': np.arange(df.shape[0])
+    ...                },
+    ... )
+    Inference...
+    """  # noqa: W605
+
+    def build_model(self, X, t, coords):
+        "Defines the PyMC propensity model"
+        with self:
+            self.add_coords(coords)
+            X_data = pm.Data("X", X, dims=["obs_ind", "coeffs"])
+            t_data = pm.Data("t", t.flatten(), dims="obs_ind")
+            b = pm.Normal("b", mu=0, sigma=1, dims="coeffs")
+            mu = pm.math.dot(X_data, b)
+            p = pm.Deterministic("p", pm.math.invlogit(mu))
+            pm.Bernoulli("t_pred", p=p, observed=t_data, dims="obs_ind")
+
+    def fit(self, X, t, coords):
+        """Draw samples from posterior, prior predictive, and posterior predictive
+        distributions. We overwrite the base method because the base method assumes
+        a variable y and we use t to indicate the treatment variable here.
+        """
+        # Ensure random_seed is used in sample_prior_predictive() and
+        # sample_posterior_predictive() if provided in sample_kwargs.
+        random_seed = self.sample_kwargs.get("random_seed", None)
+
+        self.build_model(X, t, coords)
+        with self:
+            self.idata = pm.sample(**self.sample_kwargs)
+            self.idata.extend(pm.sample_prior_predictive(random_seed=random_seed))
+            self.idata.extend(
+                pm.sample_posterior_predictive(
+                    self.idata, progressbar=False, random_seed=random_seed
+                )
+            )
+        return self.idata
 
 
 class InterventionTimeEstimator(PyMCModel):
